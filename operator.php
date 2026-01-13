fba8bf3ab4440ad0b45610a6d6965c807ab300e5
andapatyt-ctrl <andapatyt@gmail.com>
2026-01-13 13:59:30 +0700

    operator

---
+++ operator.php
@@ -0,0 +1,12 @@
+<?php
+$x = 10;
+$y = 20;
+?>
+
+<h3>Arithmetic Operators</h3> <!-- ตัวดำเนินการทางคณิตศาสตร์ -->
+<div>Addition : <?php echo $x + $y; ?></div> <!-- การบวก -->
+<div>Subtraction : <?php echo $x - $y; ?></div> <!-- การลบ -->
+<div>Multiplication : <?php echo $x * $y; ?></div> <!-- การคูณ -->
+<div>Division : <?php echo $x / $y; ?></div> <!-- การหาร -->
+<div>Modulus : <?php echo $x % $y; ?></div> <!-- หารเอาเศษ -->
+<div>Exponentiation : <?php echo $x ** $y; ?></div> <!-- การยกกำลัง -->
