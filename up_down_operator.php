4e97667cf0302c36b4a26fc12ab8bf538aec9515
andapatyt-ctrl <andapatyt@gmail.com>
2026-01-13 14:03:54 +0700

    up_down_operator

---
+++ up_down_operator.php
@@ -0,0 +1,9 @@
+<?php
+$x = 10;
+?>
+
+<h3>Up Down Operators</h3>
+<div>Up: <?php echo $x++; ?></div> <!-- ทำให้ค่าของ $x เพิ่มขึ้น 1 -->
+<div>Value: <?php echo $x; ?></div> <!-- แสดงค่าปัจจุบันของ $x -->
+<div>Down: <?php echo $x--; ?></div> <!-- ทำให้ค่าของ $x ลดลง 1 -->
+<div>Value: <?php echo $x; ?></div> <!-- แสดงค่าปัจจุบันของ $x -->
