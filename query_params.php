7f5db6530cbb0ddd5a29323f5a1f020fbaad91f5
andapatyt-ctrl <andapatyt@gmail.com>
2026-01-13 15:34:47 +0700

    query_param

--- query_params.php
+++ query_params.php
@@ -1,5 +1,6 @@
 <h3>Query Params</h3>
 <?php
-if (isset($_GET['name'])) { // ตรวจสอบว่ามีการส่งค่าช่อง name มาหรือไม่
+if (isset($_GET['name'])) { // ตรวจสอบว่ามีการส่งค่าช่อง name มาหรือไม่  //Name=Pooh
 echo $_GET ['name']; // แสดงค่าที่ส่งมาจาก URL query parameter ชื่อ name
 }
+?>
