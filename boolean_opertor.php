aaaaa3059a6b58a8147d648cdd0523a144cc05de
andapatyt-ctrl <andapatyt@gmail.com>
2026-01-13 13:52:18 +0700

    boolean_operator

---
+++ boolean_opertor.php
@@ -0,0 +1,10 @@
+<?php
+$x = 10; <!-- กำหนดค่าให้ตัวแปร $x -->
+$y = 20;
+?>
+
+<h3>Boolean Operators</h3>
+<div>Equal: <?php echo $x == $y; ?></div> <!-- เปรียบเทียบค่า -->
+<div>Not Equal: <?php echo $x != $y; ?></div> <!-- ไม่เท่ากับ -->
+<div>Greater Than: <?php echo $x > $y; ?></div> <!-- มากกว่า -->
+<div>Less Than: <?php echo $x < $y; ?></div> <!-- น้อยกว่า -->
